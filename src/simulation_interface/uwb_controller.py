4860eb99bee54e252b61a8ef127398a0475a2db9
dogukan <dogukan.altay@student.uantwerpen.be>
2022-12-03 00:56:08 +0100

    Base rosparam config added. uwb_controller and robot_controller integrated with new rabbitmq interface and gazebo.

--- src/simulation_interface/uwb_controller.py
+++ src/simulation_interface/uwb_controller.py
@@ -3,6 +3,8 @@ import os
 import pika
 import rospy
 import json
+
+from aamas_sim.src.comm_interface.rabbitmq_interface import RabbitCommunication
 from geometry_msgs.msg import Twist
 from gazebo_msgs.msg import ModelStates
 from rospy_message_converter import message_converter
@@ -11,39 +13,39 @@ from turtlebot3_msgs.msg import SensorState
 
 class UWBController:
 
-    def __init__(self, robot_id):
-        rospy.init_node('uwb_controller_%s' % robot_id)
+    def __init__(self, robot_count):
+        rospy.init_node('uwb_controller')
 
-        self.robot_id = robot_id
-        self.rabbit_channel = None
-        self.rabbit_queue_name = 'tb3_%s_uwb' % self.robot_id
+        self.robot_count = robot_count
+        self.robot_base_name = 'tb3_%s'
 
-        self.connect_to_rabbitMQ()
+        self.rabbitmq_interface = RabbitCommunication()
         self.uwb_sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self.robot_pos_callback, queue_size=1,
                                         buff_size=1)
 
-    def connect_to_rabbitMQ(self):
-        credentials = pika.PlainCredentials('dogu', 'dogu')
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
+        self.rabbit_queue_name = 'uwb'
+        self.rabbitmq_interface.register_queue(self.rabbit_queue_name)
 
-        self.rabbit_channel = connection.channel()
-
-        self.rabbit_channel.queue_declare(queue=self.rabbit_queue_name,
-                                          arguments={"x-max-length": 1,
-                                                     "x-overflow": "drop-head"})
+    def init_rabbit_queue(self):
+        queue_base_name = self.robot_base_name + '_pos'
+        for i in range(self.robot_count):
+            self.rabbitmq_interface.register_queue(queue_base_name % i)
 
     def robot_pos_callback(self, data: ModelStates):
-        robot_name = 'tb3_%s' % self.robot_id
-        pos_index = data.name.index(robot_name)
-        pos_dict = message_converter.convert_ros_message_to_dictionary(data.pose[pos_index])
+        robot_list = []
+        for i in range(self.robot_count):
+            robot_name = self.robot_base_name % i
+            pos_index = data.name.index(robot_name)
+            pos_dict = message_converter.convert_ros_message_to_dictionary(data.pose[pos_index])
+            pos_dict['robot_id'] = i
+            robot_list.append(pos_dict)
 
-        self.rabbit_channel.basic_publish(exchange='', routing_key=self.rabbit_queue_name, body=json.dumps(pos_dict))
+        self.rabbitmq_interface.send(self.rabbit_queue_name, robot_list)
 
     def run(self):
         rospy.spin()
 
 
 if __name__ == '__main__':
-    a = UWBController(0)
+    a = UWBController(3)
     a.run()
